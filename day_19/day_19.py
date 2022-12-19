e7a941f80c8588b11f322b3229332066a81c28cf
Harry Bleyan <gbleyan@gmail.com>
2022-12-19 00:32:09 -0800

    Part 1 day 19

---
+++ day_19/day_19.py
@@ -0,0 +1,138 @@
+"""Day 19."""
+import re
+from collections import defaultdict
+from functools import cache
+from tqdm import tqdm
+import multiprocessing
+
+
+
+stone_types = ['ore', 'clay', 'obsidian', 'geode']
+blueprints = {}
+# Mapping from stone to max required number to make a robot
+max_reqs = defaultdict(lambda: defaultdict(int))
+
+def get_data():
+    with open('day_19.txt', 'r') as f:
+        data = f.read().strip().split('\n')
+        results = {}
+        for i, v in enumerate(data):
+            scan = re.search(r'Blueprint\s(\d+).*?'
+                + r'Each\sore.*?(\d+)\sore.*?'
+                + r'Each\sclay.*?(\d+)\sore.*?'
+                + r'Each\sobsidian.*?(\d+)\sore.*?(\d+)\sclay.*?'
+                + r'Each\sgeode.*?(\d+)\sore.*?(\d+)\sobsidian.*?',
+                v)
+            cur_id = int(scan.group(1))
+            # Cost is (ore, clay, obsidian, geode)
+            # Robots are (ore, clay, obsidian, geode)
+            robot_cost = defaultdict(lambda: defaultdict(int))
+            # Ore
+            robot_cost[0][0] = int(scan.group(2))
+            # Clay
+            robot_cost[1][0] = int(scan.group(3))
+            # Obsidian
+            robot_cost[2][0] = int(scan.group(4))
+            robot_cost[2][1] = int(scan.group(5))
+            # Geode
+            robot_cost[3][1] = int(scan.group(6))
+            robot_cost[3][2] = int(scan.group(7))
+
+            results[cur_id] = robot_cost
+    return results
+
+
+def GetPossibleRobotsToBuild(blueprint, robots, stones):
+    possible_idx = []
+    for robot_idx in range(4):
+        for stone_idx in range(4):
+            if stones[stone_idx] < blueprint[robot_idx][stone_idx]:
+                break
+        else:
+            possible_idx.append(robot_idx)
+    return possible_idx
+
+
+def BuildRobot(robot_idx, blueprint, stones):
+    stones = list(stones)
+    for stone_idx in range(4):
+        stones[stone_idx] -= blueprint[robot_idx][stone_idx]
+    return tuple(stones)
+
+
+def HarvestAllRobots(robots, stones):
+    stones = list(stones)
+    for robot_idx, num_robots in enumerate(robots):
+        stones[robot_idx] += num_robots
+    return tuple(stones)
+
+
+@cache
+def RecursivelySearchMaxGeodes(blueprint_id, cycles, robots, stones):
+    blueprint = blueprints[blueprint_id]
+    max_geodes = stones[-1]
+    if not cycles:
+        return max_geodes
+
+    # Try building a robot
+    for robot_idx in GetPossibleRobotsToBuild(blueprint, robots, stones):
+        # Check if we're at max required robot capacity
+        if max_reqs[blueprint_id][robot_idx] <= robots[robot_idx]:
+            # If the maximum per-turn number of required stones is equal or is
+            # less than the number of existing per-turn production units, prune
+            # the tree here.
+            continue
+        new_stones = BuildRobot(robot_idx, blueprint, stones)
+        # Harvest using existing robots
+        new_stones = HarvestAllRobots(robots, new_stones)
+        # Add the robot to the fleet
+        new_robots = list(robots)
+        new_robots[robot_idx] += 1
+        new_robots = tuple(new_robots)
+        max_geodes = max(max_geodes, RecursivelySearchMaxGeodes(blueprint_id, cycles - 1, new_robots, new_stones))
+
+    # Harvest using existing robots
+    new_stones = HarvestAllRobots(robots, stones)
+    max_geodes = max(max_geodes, RecursivelySearchMaxGeodes(blueprint_id, cycles - 1, robots, new_stones))
+
+    return max_geodes
+
+
+def part_1():
+    data = get_data()
+    for i, b in data.items():
+        blueprints[i] = b
+        # Update the maximum required stones
+        for robot_idx in range(4):
+            for stone_idx in range(3):
+                max_reqs[i][stone_idx] = max(max_reqs[i][stone_idx], b[robot_idx][stone_idx])
+            max_reqs[i][3] = float('inf')
+
+    # Ore, clay, obsidian, geode
+    robots = (1, 0, 0, 0,)
+    stones = (0, 0, 0, 0,)
+    
+    max_blueprint = None
+    max_geodes = float('-inf')
+    result = 0
+    for blueprint_id in tqdm(blueprints):
+        cur_geodes = RecursivelySearchMaxGeodes(
+            blueprint_id, 
+            cycles = 24,
+            robots = robots,
+            stones = stones)
+        if cur_geodes > max_geodes:
+            max_blueprint = blueprint_id
+            max_geodes = cur_geodes
+        result += blueprint_id * cur_geodes
+    print(blueprint_id, max_geodes)
+    print(result)
+
+
+def part_2():
+    data = get_data()
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
