71723b45a2989b8f2ced544cd7ff74e4021287e5
Harry Bleyan <gbleyan@gmail.com>
2022-12-22 08:07:58 -0500

    Add blank day template

---
+++ blank.py
@@ -0,0 +1,21 @@
+"""Day XXX."""
+
+
+def get_data():
+    with open('day_XXX.txt', 'r') as f:
+        data = f.read().strip().split('\n')
+
+    return data
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
